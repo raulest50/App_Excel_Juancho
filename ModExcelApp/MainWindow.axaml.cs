e8ba1e8b6c0159664376139f3291e0a17f96f71d
raulest50 <raulest50@gmail.com>
2024-05-29 14:23:20 -0500

    se empezaron a agregar elementos UI

--- ModExcelApp/MainWindow.axaml.cs
+++ ModExcelApp/MainWindow.axaml.cs
@@ -18,5 +18,33 @@ namespace ModExcelApp
             await dialog.ShowDialog(this);
 
         }
+
+
+        public double ctoCol(double ctoRMB, double tasa, double cbmCaja, double tasa_m3, double unds_caja)
+        {
+            return ctoRMB * tasa * 1.05 + cbmCaja * tasa_m3 / unds_caja;
+        }
+
+        public double cto_place(double cto_col, string bodega)
+        {
+            double cto = 1;
+            switch(bodega)
+            {
+                case "MDH":
+                    cto = 0.7;
+                    break;
+                case "TIN":
+                    cto = 0.75;
+                    break;
+                case "LIKE":
+                    cto= 0.65;
+                    break;
+                case "ALMA":
+                    cto= 0.7;
+                    break;
+            }
+
+            return cto_col*2*cto;
+        }        
     }
 }
