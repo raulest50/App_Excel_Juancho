235a35ee2c066a343d2accf672615efa7e38ec23
raulest50 <raulest50@gmail.com>
2024-05-30 17:06:35 -0500

    se agregaron mas rutinas

--- ModExcelApp/MainWindow.axaml.cs
+++ ModExcelApp/MainWindow.axaml.cs
@@ -1,29 +1,83 @@
 using Avalonia.Controls;
 using OfficeOpenXml;
+using System.IO;
+using System.Linq;
+
+using System.Threading.Tasks;
 
 namespace ModExcelApp
 {
+
+
+    /*
+     * Marca: A
+     * Foto: C-G
+     * CTNS: U
+     * Cantidad:V
+     * Precio RMB:W
+     * 
+     * Total RMB: AD
+     * CMB Caja: AI
+     * CMB Total: AJ
+     * 
+     * --------
+     * 
+     * cto: AO
+     * sugerido: AP
+     * 
+     */
+
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            InitDefaultAllTextBoxes();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
         }
 
-        private async void OnStart_ButtonStart(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private async void HandlerStart(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            //var vp_mdf = pMDF.Text;
-
-            //var dialog = new ModalDialog();
-            //await dialog.ShowDialog(this);
-
+            string fpath = tb_file_path.Text;
+            await ProcesarExcel(fpath);
         }
 
-
-        public double ctoCol(double ctoRMB, double tasa, double cbmCaja, double tasa_m3, double unds_caja)
+        private async void HandlerBrowse(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            return ctoRMB * tasa * 1.05 + cbmCaja * tasa_m3 / unds_caja;
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filters.Add(new FileDialogFilter() { Name = "Excel Files", Extensions = { "xlsx", "xls" } });
+
+            var result = await openFileDialog.ShowAsync(this);
+            if (result != null && result.Length > 0)
+            {
+                tb_file_path.Text = result[0];
+            }
         }
+
+
+        // funcion principal
+        public async Task ProcesarExcel( string file_path)
+        {
+            using (var package = new ExcelPackage(new FileInfo(file_path)))
+            {
+                ExcelWorkbook wb = package.Workbook;
+                DeleteAllExceptDespacho(wb);
+
+                ExcelWorksheet mdh, tin, like, alma;
+
+                mdh = CreateSheetIfNotExist(wb, "MDH");
+                tin = CreateSheetIfNotExist(wb, "TIN");
+                like = CreateSheetIfNotExist(wb, "LIKE");
+                alma = CreateSheetIfNotExist(wb, "ALMA");
+
+                // Save the changes to the file
+                await Task.Run(() => package.Save());
+            }
+
+        }
+
 
         public double cto_place(double cto_col, string bodega)
         {
@@ -45,6 +99,70 @@ namespace ModExcelApp
             }
 
             return cto_col*2*cto;
-        }        
+        }
+
+        public ExcelWorkbook LoadWorkbook(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath) || !File.Exists(filePath))
+            {
+                // Handle error: file path is invalid or file does not exist
+                return null;
+            }
+
+            var package = new ExcelPackage(new FileInfo(filePath));
+            return package.Workbook;
+        }
+
+
+        public static void DeleteAllExceptDespacho(ExcelWorkbook workbook)
+        {
+            // Disable display alerts
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // Collect all sheets except the one named "Despacho"
+            var sheetsToDelete = workbook.Worksheets
+                .Where(sheet => sheet.Name != "Despacho")
+                .ToList();
+
+            // Delete collected sheets
+            foreach (var sheet in sheetsToDelete)
+            {
+                workbook.Worksheets.Delete(sheet);
+            }
+        }
+        
+        public static ExcelWorksheet CreateSheetIfNotExist(ExcelWorkbook workbook, string sheetName)
+        {
+            // Check if the sheet already exists
+            var existingSheet = workbook.Worksheets.FirstOrDefault(ws => ws.Name == sheetName);
+            if (existingSheet != null)
+            {
+                return existingSheet;
+            }
+
+            // If the sheet does not exist, create it
+            var newSheet = workbook.Worksheets.Add(sheetName);
+            return newSheet;
+        }
+
+        private void InitDefaultAllTextBoxes()
+        {
+            tb_col_marca.Text = "A";
+            tb_col_foto_i.Text = "C";
+            tb_col_foto_e.Text = "G";
+            tb_col_ctns.Text = "U";
+            tb_col_cantidad.Text = "V";
+            tb_col_precio_rmb.Text = "W";
+            tb_col_total_rmb.Text = "AD";
+            tb_cbm_caja.Text = "AI";
+            tb_col_cbm_total.Text = "AJ";
+            //tb_cto_mdh.Text = "";
+            //tb_cto_tin.Text ="";
+            //tb_cto_like.Text = "";
+            //tb_cto_alma.Text = "";
+        }
+
+
+
     }
 }
---
+++ ModExcelApp/Rutinas.cs
@@ -0,0 +1,99 @@
+﻿using OfficeOpenXml.Style;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ModExcelApp
+{
+    public static class Rutinas
+    {
+
+        public static void SetupColTitles(ExcelWorksheet hojaMain, ExcelWorksheet otherSheet)
+        {
+            if (hojaMain == null || otherSheet == null)
+            {
+                throw new ArgumentNullException("One or more invalid worksheet references provided.");
+            }
+
+            otherSheet.Row(1).Height = 59;
+
+            // Set the width of columns in otherSheet
+            otherSheet.Column(2).Width = 24; // foto1
+            otherSheet.Column(3).Width = 24;
+            otherSheet.Column(4).Width = 24;
+            otherSheet.Column(5).Width = 24;
+            otherSheet.Column(6).Width = 24; // col width foto5
+
+            otherSheet.Column(10).Width = 20;
+            otherSheet.Column(11).Width = 20;
+            otherSheet.Column(12).Width = 20;
+            otherSheet.Column(13).Width = 20;
+            otherSheet.Column(14).Width = 20;
+            otherSheet.Column(15).Width = 20;
+
+            // Copy the content from specified cells in hojaMain to otherSheet
+            hojaMain.Cells[3, 1].Copy(otherSheet.Cells[1, 1]); // marca
+            hojaMain.Cells[3, 3].Copy(otherSheet.Cells[1, 2]); // foto1
+            hojaMain.Cells[3, 4].Copy(otherSheet.Cells[1, 3]);
+            hojaMain.Cells[3, 5].Copy(otherSheet.Cells[1, 4]);
+            hojaMain.Cells[3, 6].Copy(otherSheet.Cells[1, 5]);
+            hojaMain.Cells[3, 7].Copy(otherSheet.Cells[1, 6]); // foto5
+
+            hojaMain.Cells[3, 21].Copy(otherSheet.Cells[1, 7]); // CTNS
+            hojaMain.Cells[3, 22].Copy(otherSheet.Cells[1, 8]); // Cantidad
+            otherSheet.Cells[1, 8, 1, 9].Merge = true; // Merge H1:I1
+            otherSheet.Cells[1, 8].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+            hojaMain.Cells[3, 24].Copy(otherSheet.Cells[1, 10]); // Precio RMB
+
+            hojaMain.Cells[3, 30].Copy(otherSheet.Cells[1, 11]); // Total
+            hojaMain.Cells[3, 35].Copy(otherSheet.Cells[1, 12]); // CBM Caja
+            hojaMain.Cells[3, 36].Copy(otherSheet.Cells[1, 13]); // CBM total
+
+            // Copy only format
+            CopyCellFormatting(hojaMain.Cells[3, 30], otherSheet.Cells[1, 14]);
+            CopyCellFormatting(hojaMain.Cells[3, 30], otherSheet.Cells[1, 15]);
+            CopyCellFormatting(hojaMain.Cells[3, 30], otherSheet.Cells[1, 16]);
+
+            //hojaMain.Cells[3, 30].Copy(otherSheet.Cells[1, 14], ExcelCopyOption.CopyFormats);
+            //hojaMain.Cells[3, 30].Copy(otherSheet.Cells[1, 15], ExcelCopyOption.CopyFormats);
+            //hojaMain.Cells[3, 30].Copy(otherSheet.Cells[1, 16], ExcelCopyOption.CopyFormats);
+
+            otherSheet.Cells[1, 14].Value = "COSTO";
+            otherSheet.Cells[1, 15].Value = "SUGERIDO";
+        }
+
+
+
+
+        private static void CopyCellFormatting(ExcelRange sourceCell, ExcelRange targetCell)
+        {
+            targetCell.Style.Numberformat.Format = sourceCell.Style.Numberformat.Format;
+            targetCell.Style.Font.Name = sourceCell.Style.Font.Name;
+            targetCell.Style.Font.Size = sourceCell.Style.Font.Size;
+            targetCell.Style.Font.Bold = sourceCell.Style.Font.Bold;
+            targetCell.Style.Font.Italic = sourceCell.Style.Font.Italic;
+            targetCell.Style.Font.UnderLine = sourceCell.Style.Font.UnderLine;
+            targetCell.Style.Fill.PatternType = sourceCell.Style.Fill.PatternType;
+
+            Color bgcolor = ColorTranslator.FromHtml(sourceCell.Style.Fill.BackgroundColor.Rgb);
+            targetCell.Style.Fill.BackgroundColor.SetColor(bgcolor);
+
+            targetCell.Style.Border.Left.Style = sourceCell.Style.Border.Left.Style;
+            targetCell.Style.Border.Right.Style = sourceCell.Style.Border.Right.Style;
+            targetCell.Style.Border.Top.Style = sourceCell.Style.Border.Top.Style;
+            targetCell.Style.Border.Bottom.Style = sourceCell.Style.Border.Bottom.Style;
+            targetCell.Style.HorizontalAlignment = sourceCell.Style.HorizontalAlignment;
+            targetCell.Style.VerticalAlignment = sourceCell.Style.VerticalAlignment;
+        }
+    }
+
+
+
+
+
+}
